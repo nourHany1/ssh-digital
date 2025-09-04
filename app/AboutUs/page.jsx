ab0dd4e824239d41f229555a5d31ba725551327e
nourHany1 <162379890+nourHany1@users.noreply.github.com>
2025-09-04 05:02:37 +0300

    feat: first commit

---
+++ app/AboutUs/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AboutUs() {
+  return (
+    <div className='mt-36' >
+      <h1 className='text-center text-white' >About Us</h1>
+    </div>
+  )
+}
