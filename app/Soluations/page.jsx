d8e3de3eeb492f1f3c15766c67152f61d9b59ddd
nourHany1 <162379890+nourHany1@users.noreply.github.com>
2025-09-04 06:03:03 +0300

    feat: first commit

---
+++ app/Soluations/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Soluations() {
+  return (
+    <div className='mt-36' >
+      <h1 className="text-white text-center " >Soluations</h1>
+    </div>
+  )
+}
